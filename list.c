08de2a3c4121efa2458bfd52ed6296014a9175bd
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2017-09-10 15:15:43 -0400

    Added documentation and created testing code in list_test.c

--- list.c
+++ list.c
@@ -10,68 +10,93 @@ list* create_list() {
 	return temp;
 }
 
+
+/* A helper function that checks to see if the list pointer is NULL or list is empty.
+ * Returns 0 if ll 0r ll -> head is not null, -1 if otherwise. */
+int check_list_null(list* ll)  {
+	if(ll == NULL || ll -> head == NULL) {
+		printf("\n*** List is NULL or empty***\n");
+		return -1;
+	}
+	return 0;
+}
+
 /* Adds item to start of the list. Allocates a
  * new buffer and copies the string from item (use malloc,
  * strlen, and strncpy; or try strdup).
  * Returns 0 if successful, non-zero otherwise. */
 int add_to_list(list* ll, char* item) {
-	//If it is the first element to add in array
-
+	//If list is null or the string is null, return -1
 	if(item == NULL || ll == NULL) {
 		printf("\n*** The list or item is NULL ***\n");
 		return -1;
 	}
-
+	//if the memory allocation fails for creating node
 	struct node* newNode = (struct node*) malloc(sizeof(struct node));
 	if(newNode == NULL) {
 		printf("\nError creating new node!\n");
 		return -1;
 	}
 
-	newNode -> next = ll -> head;
+	newNode -> next = ll -> head; //change the next of new node created to the head
 
 	char* character = malloc (strlen(item) + 1);
+	//if memory allocation fails for creating a buffer for the string
 	if(character == NULL) {
 		printf("\n*** Error creating buffer for the string ***\n");
-		return -1;//Memory allocation failed
+		return -1;
 	}
-	strncpy(character, item, strlen(item));
+	strncpy(character, item, strlen(item) + 1); //copy from item.
 
 	newNode -> ch = character;
-	ll -> head = newNode;
-	return 0;
+	ll -> head = newNode; //change the head to the new element
+	return 0; //return if successful
 }
 
 /* Removes the string from the front of the list and
  * returns a pointer to it. The caller is expected to free
  * the string returned when finished with it. */
 char* remove_from_list(list* ll) {
-	if(ll == NULL) {
-		printf("\n*** List is NULL ***\n");
+	if(check_list_null(ll) < 0) 
 		return NULL;
-	}
-	struct node* newNode = (struct node*) malloc(sizeof(struct node));
-	newNode = ll -> head;
+
+	struct node* newNode = ll -> head;
+	ll -> head = newNode -> next; //change the head to point to it's next node element.
+	newNode -> next = NULL;
 	char* deleteChar = newNode -> ch;
-	ll -> head = newNode -> next;
-	free(newNode);
-	return deleteChar;
+	free(newNode); //free the node
+	return deleteChar; //return the character that was deleted
 }
+
 
 /* Prints every string in the list, with a new line
  * character at the end of each string */
 void print_list(list *ll) {
-	if(ll == NULL || ll -> head == NULL) {
-		printf("\n*** List is NULL or empty ***\n");
+	if(check_list_null(ll) < 0) 
 		return;
-	}
-	struct node* iter = ll -> head;
-	printf("\n");
-	while(iter != NULL) {
+
+	for(struct node* iter = ll -> head; iter != NULL; iter = iter -> next) {
 		printf("%s\n", iter -> ch);
-		iter = iter -> next;
 	}
-	printf("\n");
+}
+
+/* A helper function which is used to free all nodes in the list.
+ * This is used by flush_list(ll) and free_list(ll) */
+void delete_list(list* ll) {
+	if(check_list_null(ll) < 0) 
+		return;
+
+	struct node* iter = ll -> head;
+	while(iter != NULL) {
+		struct node* nextNode = iter -> next; //store the next node
+		char* freedValue = iter -> ch;
+
+		free(freedValue); //free the character
+		free(iter); //free the node
+
+		iter = nextNode; //change the current node to next
+		ll -> head = iter; //changing the head at every step
+	}
 }
 
 /* Flushes (clears) the entire list and re-initializes the
@@ -79,37 +104,13 @@ void print_list(list *ll) {
  * valid, empty list when this function returns. Any memory
  * allocated to store items in the list should be freed. */
 void flush_list(list* ll) {
-	if(ll == NULL) {
-		printf("\n*** List is NULL ***\n");
-		return;
-	}
-	struct node* iter = ll -> head;
-	while(iter != NULL) {
-		struct node* currentNode = iter;
-		free(currentNode);
-		currentNode = NULL;
-		iter = iter -> next;
-	}
-	ll -> head = NULL;
-
+	delete_list(ll); //use the helper function
 }
 
 /* De-allocates all data for the list. Ensure all memory
  * allocated for this list is freed, including any
  * allocated strings and the list itself. */
 void free_list(list *ll) {
-	if(ll == NULL) {
-		printf("\n*** List is NULL ***\n");
-		return;
-	}
-	struct node* iter = ll -> head;
-
-	while(iter != NULL) {
-		struct node* currentNode = iter;
-		free(currentNode);
-		currentNode = NULL;
-		iter = iter -> next; 
-	}
-	free(ll);
-	ll -> head = NULL;
+	delete_list(ll); //use the helper function
+	free(ll); //ensuring the list itself is freed
 }
--- list_test.c
+++ list_test.c
@@ -5,32 +5,77 @@ int main() {
 
 	char* firstCharacter = "Sudheesh";
 	char* secondCharacter = "Bhattarai";
+	char* testing = NULL;
+	char* thirdCharacter = "Linked List";
 
+	/* Testing code for add_to_list */
+
+	//adding the elements in the list
+	printf("\n######################## Code and testing for add_to_list ########################\n");
 	int value = add_to_list(head, firstCharacter);
 	value = add_to_list(head, secondCharacter);
+	printf("\n\t\t-----> Printing after adding <-----\n");
 	print_list(head);
+	//testing to see if the function complains when NULL is passed as character and list
+	value = add_to_list(head, testing);
+	value = add_to_list(NULL, testing);
 
+
+	/* Testing code for remove_from_list */
+
+	//removing the first element from the list
+	printf("\n######################## Code and testing for remove_from_list ########################\n");
 	char* removeCharacter = remove_from_list(head);
 	free(removeCharacter);
+	printf("\n\t\t-----> Printing after removing first element <-----\n");
 	print_list(head);
+	//deleting the element from the list again, the list should be empty.
+	removeCharacter = remove_from_list(head);
+	free(removeCharacter);
+	printf("\n\t\t-----> Printing after removing second element<-----\n");
+	print_list(head);
+	//tesing to see if the function complains when flushing from empty list
+	printf("\n\t\t-----> Testing if removeCharacter complains when trying to remove from empty list <-----\n");
+	removeCharacter = remove_from_list(head);
+	free(removeCharacter);
 
+
+	/* Testing code for flush_list */
+
+	//adding elements in the list again
+	printf("\n######################## Code and testing for flush_list ########################\n");
+	value = add_to_list(head, firstCharacter);
+	value = add_to_list(head, secondCharacter);
+	printf("\n\t\t-----> Printing after adding element before flushing <-----\n");
+	print_list(head);
 	flush_list(head);
+	printf("\n\t\t-----> Printing after flushing <-----\n");
 	print_list(head);
+	//tesing to see if the function complains when flushing from empty list
+	printf("\n\t\t-----> Testing if flush_list complains when trying to flush empty list <-----\n");
+	flush_list(head);
 
-	char* thirdCharacter = "Linked List";
+	/* Testing code for free_list */
+	
+	//adding element to list again
+	printf("\n######################## Code and testing for free_list ########################\n");
 	value = add_to_list(head, thirdCharacter);
 	value = add_to_list(head, secondCharacter);
 	value = add_to_list(head, firstCharacter);
+	printf("\n\t\t-----> Printing after adding element before freeing <-----\n");
 	print_list(head);
-
 	free_list(head);
+	printf("\n\t\t-----> Printing after freeing <-----\n");
 	print_list(head);
 
+	//initialising the head again
+	printf("\n######################## Final printing the list ########################\n");
 	head = create_list();
 	value = add_to_list(head, thirdCharacter);
 	value = add_to_list(head, secondCharacter);
 	value = add_to_list(head, thirdCharacter);
+	printf("\n\t\t-----> Printing after reinitialising head and adding <-----\n");
 	print_list(head);
-
+	printf("\n\n");
 	return 0;
 }
